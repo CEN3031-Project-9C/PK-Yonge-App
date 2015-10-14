9efa569b6b9042195d698cc0457dd17cb0526603
Cody Fitzpatrick <cody@orbitalone.com>
2015-10-14 10:51:04 -0400

    Updated MongoDB Connection String (Again) And Renamed Test Portal Directory

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://9c-team:g*8D30$Dfl23r$@azd#@ds051923.mongolab.com:51923/pk-yonge',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://9c-team:g8D30Dfl23razd@ds051923.mongolab.com:51923/pk-yonge',
     options: {
       user: '',
       pass: ''
