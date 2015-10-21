bf9372f024c38f4d065953f3d5ca0cd146e203d4
Nicola37 <nicolafrachesen@bellsouth.net>
2015-10-21 14:25:43 -0400

    Removing incorrect js files.

--- modules/test_portal/client/js/login.js
+++
@@ -1,7 +0,0 @@
-$("#login-pk").click(function() {
-	if($("#studentid").val() && $("#pw").val() && $("#verified-info").is(':checked')) {
-		location.href = '../../../choose-test/client/views/index.html';
-	} else {
-		$("#incomplete-login-form").fadeIn();
-	}
-});
