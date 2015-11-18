1cedcbab664cf82c637bdc698808d6ec34c6106f
Nicola37 <nicolafrachesen@bellsouth.net>
2015-11-18 16:22:30 -0500

    Changing gitignore and production.

--- config/env/production.js
+++ config/env/production.js
@@ -8,10 +8,10 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://9cUser:4LdamoeAd@ds051923.mongolab.com:51923/pk-yonge',
     options: {
-      user: '',
-      pass: ''
+      user: '9cUser',
+      pass: '4LdamoeAd'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
