801585212aa3695094513948946de71a2998ba7c
Xiaoxi <shoush0713@gmail.com>
2015-11-23 17:46:47 -0500

    added progress bar, need to integrade with choose-test module so the timer starts counting when ng-clicked() start-test

--- modules/test_portal/client/controllers/test_portal.client.controller.js
+++ modules/test_portal/client/controllers/test_portal.client.controller.js
@@ -1,5 +1,52 @@
 'use strict';
 
+
+angular.module('test_portal')
+	.directive('ngProgressbar', function($timeout) {
+		return {
+
+			restrict : 'E',
+			template : "<div class='progress'><div class='progress-bar progress-bar-{{type}}' role='progressbar' style='width:{{percent_complete}}%;'> {{percent_complete}} % </div></div>",
+			replace : true,
+			scope: {
+            	max: '@',
+            	start: '@',
+            	type: '@',
+            	onStop: '&'
+        	},
+
+        	controller : ['$scope', '$timeout', function($scope, $timeout) {
+        		$scope.onTimeout = function() {
+					$scope.startAt++;
+					$scope.current = $scope.startAt;
+					$scope.mytimeout = $timeout($scope.onTimeout,1000);
+					if($scope.startAt >= $scope.max) {
+						$scope.stop();
+						$scope.onStop();
+					}
+					$scope.percent_complete = Number(100 * $scope.current / $scope.max);
+				};
+
+				$scope.stop = function() {
+					$timeout.cancel($scope.mytimeout);
+				};
+        	}],
+
+			link : function (scope, elem, attrs, ctrl) {
+
+				attrs.$observe('start', function(value) {
+					console.log('observe start change');
+					if(value === 'true') {
+						scope.startAt = 0;
+						scope.mytimeout = $timeout(scope.onTimeout,1000);
+					}else if(value ==='false') {
+						scope.stop();
+					}	
+				});
+			}
+		};
+	});
+
 // Questions controller
 angular.module('test_portal').controller('QuestionsController', [
 	'$scope', 
@@ -126,7 +173,19 @@ angular.module('test_portal').controller('QuestionsController', [
 			//save back to DB
 		};
 		$scope.openCalcWindow = function(){
-			var myWindow = window.open("calculator", "calcWindow", "scrollbars=yes, resizable=yes, top=200, left=700, width=425, height=450");
+			var myWindow = window.open("calculator", "calcWindow", "resizable=0, location=no,menubar=no,status=no,top=200, left=700, width=425, height=450");
+		};
+
+		$scope.timer_running = false;
+		$scope.max_count = 100;
+
+		$scope.startProgress = function() {
+			$scope.timer_running = true;
+		 };
+
+		$scope.stopProgress = function(){
+		    $scope.timer_running = false;
+		    //pop up modal to pause the test...
 		};
 	}
 ]);
